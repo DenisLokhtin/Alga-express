48f02a1c844fbe921d1b1e842a7e373cf753a98c
Shabdan <schabkul@gmail.com>
2022-01-14 18:42:44 +0600

    #2 Обновлен app.js добавлен layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
+import Layout from "./components/UI/Layout/Layout";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' exact element={<Register/>}/>
-        </Routes>
+        <Layout>
+            <Routes>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/register' exact element={<Register/>}/>
+            </Routes>
+        </Layout>
     );
 };
 
