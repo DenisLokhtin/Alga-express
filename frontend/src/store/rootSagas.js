0dd7389440525a7adb535c937468ebb8586e47b3
Shevchenko Timofei <tima9953@gmail.com>
2022-01-14 18:41:42 +0600

    Исправлен импорт

--- frontend/src/store/rootSagas.js
+++ frontend/src/store/rootSagas.js
@@ -1,8 +1,8 @@
 import {all} from 'redux-saga/effects';
-import registerUserSaga from "./sagas/usersSagas";
+import usersSagas from "./sagas/usersSagas";
 
 export function* rootSagas() {
     yield all([
-        ...registerUserSaga
+        ...usersSagas,
     ])
 }
