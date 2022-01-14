533bd6f68de487b65a4295e700ec846e2031d56a
Lokhtin Denis <denis.lokhtin@gmail.com>
2022-01-14 19:49:43 +0600

    заполнил Контакты #10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
+import Layout from "./components/UI/Layout/Layout";
+import AboutUs from "./containers/AboutUs/AboutUs";
+import Contacts from "./containers/Contacts/Contacts";
+import Rules from "./containers/Rules/Rules";
+import ProhibitedGoods from "./containers/ProhibitedGoods/ProhibitedGoods";
+import HowItWorks from "./containers/HowItWorks/HowItWorks";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' exact element={<Register/>}/>
-        </Routes>
+        <Layout>
+            <Routes>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/rules' exact element={<Rules/>}/>
+                <Route path='/about_us' exact element={<AboutUs/>}/>
+                <Route path='/contacts' exact element={<Contacts/>}/>
+                <Route path='/register' exact element={<Register/>}/>
+                <Route path='/how_it_works' exact element={<HowItWorks/>}/>
+                <Route path='/prohibited_goods' exact element={<ProhibitedGoods/>}/>
+            </Routes>
+        </Layout>
     );
 };
 
--- frontend/src/containers/Contacts/Contacts.js
+++ frontend/src/containers/Contacts/Contacts.js
@@ -5,9 +5,9 @@ import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles(theme => ({
     paper: {
-            display: 'flex',
-            justifyContent: 'center',
-            margin: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+        margin: '10px',
     },
 }));
 
@@ -15,14 +15,14 @@ const Contacts = () => {
     const classes = useStyles();
 
     return (
-        <Container style={{'border': '2px solid grey', 'borderRadius': '3px', 'background': '#FAFAFA'}} component='div'>
+        <Container style={{'border': '2px solid #2E2E2E', 'borderRadius': '3px', 'marginTop': '5px'}} component='div'>
             <div className={classes.paper}>
                 <Typography component="h1" variant="h3">
                     Контакты
                 </Typography>
             </div>
             <Grid component='div'>
-                <div>
+                <Typography component="div">
                     <Typography style={{'fontWeight': 'Bold'}} component="p">
                         Бишкек
                     </Typography>
@@ -40,7 +40,7 @@ const Contacts = () => {
                     </Typography>
                     <Typography component="div">
                         <Typography component="p">
-                           График работы:
+                            График работы:
                         </Typography>
                         <ul style={{'margin': '0px'}}>
                             <li>Пн: 11:00-18:00</li>
@@ -52,16 +52,38 @@ const Contacts = () => {
                             <li>Вс: 11:00-15:00</li>
                         </ul>
                     </Typography>
-                    <Typography style={{'fontWeight': 'Bold'}} component="p">
-                       Склад в Китае
+                    <Typography style={{'fontWeight': 'Bold', 'marginTop': '10px'}} component="p">
+                        Склад в Китае
                     </Typography>
-                    <Typography style={{'fontWeight': 'Bold'}} component="p">
-                       Склад в Турции
+                    <Typography style={{'fontWeight': 'Bold', 'marginTop': '10px'}} component="p">
+                        Склад в Турции
                     </Typography>
-                    <Typography style={{'fontWeight': 'Bold'}} component="p">
-                       Склад в США
+                    <Typography component="div">
+                        <Typography component="div">Заказчик: Аида</Typography>
+                        <Typography component="div">Sehir: Istanbul</Typography>
+                        <Typography component="div">Adress: Langa hisari cad 46 (Alga Express Kargo)</Typography>
+                        <Typography component="div">Ilce: Fatih</Typography>
+                        <Typography component="div">Mahallesi: Katipkasim</Typography>
+                        <Typography component="div">Post kod: 34130</Typography>
+                        <Typography component="div">Tel: 05550206083</Typography>
+                        <Typography component="div">Yenikapi: laleli</Typography>
+                        <Typography component="div">Ad: Ваше имя</Typography>
+                        <Typography component="div">Soyad: Фамилия</Typography>
                     </Typography>
-                </div>
+                    <Typography style={{'fontWeight': 'Bold', 'marginTop': '10px'}} component="p">
+                        Склад в США
+                    </Typography>
+
+                    <Typography component="div">
+                        <Typography component="div">Получатель:  *Имя Фамилия* латиницей</Typography>
+                        <Typography component="div">Адресная строка 1:  *41B Germay Drive*</Typography>
+                        <Typography component="div">Адресная строка 2:  *ALGA-KG1* указывать обязательно!</Typography>
+                        <Typography component="div">Город: *Wilmington*</Typography>
+                        <Typography component="div">Штат: *DE (Delaware)*</Typography>
+                        <Typography component="div">Почтовый код: *19804*</Typography>
+                        <Typography component="div">Телефон: *+1 (302) 669-1014*</Typography>
+                    </Typography>
+                </Typography>
             </Grid>
         </Container>
     )
