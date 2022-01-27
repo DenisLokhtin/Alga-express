3d7ab7ecdb423555f7b8dd527243b377c60040ed
Shabdan <schabkul@gmail.com>
2022-01-27 19:34:59 +0600

    #18 Созданный компонент рендерит список карточек с данными о рейсе

--- frontend/src/containers/FlightsList/FlightsList.js
+++ frontend/src/containers/FlightsList/FlightsList.js
@@ -4,6 +4,9 @@ import {useDispatch, useSelector} from "react-redux";
 import {getFlightsRequest} from "../../store/actions/flightActions";
 import Grid from "@mui/material/Grid";
 import FlightListItem from "../../components/FlightListItem/FlightListItem";
+import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const FlightsList = () => {
     const dispatch = useDispatch();
@@ -16,16 +19,60 @@ const FlightsList = () => {
     return (
         <Container>
             <h1>Рейсы</h1>
-            <Grid container flexDirection='column'  spacing={3}>
-                {flights.length !== 0 ?
-                    flights.map(item => (
-                        <FlightListItem key={item._id} flight={item} id={item._id}/>
-                    ))
-                    :
-                    <Grid item>
-                        <h2>Нет активных рейсов</h2>
-                    </Grid>
-                }
+            <Grid container flexDirection='column' spacing={4}>
+                <Grid item>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Активные рейсы</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Grid container flexDirection='column'  spacing={3}>
+                                {flights.length !== 0 ?
+                                    flights.map(item => (
+                                        item.status === 'ACTIVE'
+                                            &&
+                                        <FlightListItem key={item._id} flight={item} id={item._id}/>
+                                    ))
+                                    :
+                                    <Grid item>
+                                        <h2>Нет активных рейсов</h2>
+                                    </Grid>
+                                }
+                            </Grid>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid>
+
+                <Grid item>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Завершенные рейсы</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Grid container flexDirection='column'  spacing={3}>
+                                {flights.length !== 0 ?
+                                    flights.map(item => (
+                                        item.status === 'DONE'
+                                            &&
+                                        <FlightListItem key={item._id} flight={item} id={item._id}/>
+                                    ))
+                                    :
+                                    <Grid item>
+                                        <h2>Нет активных рейсов</h2>
+                                    </Grid>
+                                }
+                            </Grid>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid>
             </Grid>
         </Container>
     );
