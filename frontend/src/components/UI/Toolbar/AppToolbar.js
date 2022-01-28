c0cdc7f89ca3e2ab762bce2d5f2d0680c3cceafc
Shabdan <schabkul@gmail.com>
2022-01-29 04:59:29 +0600

    #37 Переделан AppToolbar, использован material ui

--- frontend/src/components/UI/Toolbar/AppToolbar.js
+++ frontend/src/components/UI/Toolbar/AppToolbar.js
@@ -16,6 +16,8 @@ import PersonIcon from '@mui/icons-material/Person';
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import {Logout} from "@mui/icons-material";
 import {logout} from "../../../store/actions/usersActions";
+import UserMenu from "./Menu/UserMenu";
+import Anonymous from "./Menu/Anonymous";
 
 const pages = [
     {url: '/rules', title: 'правила'},
@@ -25,17 +27,6 @@ const pages = [
     {url: '/news', title: 'новости'},
     {url: '/sites', title: 'сайты'},
     {url: '/faq', title: 'FAQ'},
-];
-
-const userSettings = [
-    {url: '', title: 'Личный кабинет', icon: <ManageAccountsIcon sx={{fontSize: 30}}/>},
-    {url: '/orders/history', title: 'История заказов', icon: <HistoryIcon sx={{fontSize: 30}}/>},
-    {url: '/package/register', title: 'Оформить заказ', icon: <AddIcon sx={{fontSize: 30}}/>},
-];
-
-const adminSettings = [
-    {url: '/flights', title: 'Рейсы', icon: <FlightIcon sx={{fontSize: 30}}/>},
-    {url: '/newFlight', title: 'Добавить рейс', icon: <AddIcon sx={{fontSize: 30}}/>}
 ];
 
 const useStyles = makeStyles({
@@ -105,7 +96,7 @@ const AppToolbar = () => {
                         >
                             <MenuIcon />
                         </IconButton>
-                        <BurgerMenu open={openNav} setOpen={setOpenNav}/>
+                        <BurgerMenu open={openNav} setOpen={setOpenNav} pages={pages}/>
                     </Box>
 
                     <Typography
@@ -133,83 +124,29 @@ const AppToolbar = () => {
                     </Box>
 
                     <Box sx={{ flexGrow: 0 }}>
-                        <Button
-                            className={classes.userBtn}
-                            color='inherit'
-                            size='large'
-                            onClick={handleClick}
-                            startIcon={<PersonIcon/>}
-                        >
-                            {user?.name}
-                        </Button>
-                        <Menu
-                            anchorEl={anchorEl}
-                            id="account-menu"
-                            open={open}
-                            onClose={handleClose}
-                            onClick={handleClose}
-                            PaperProps={{
-                                elevation: 0,
-                                sx: {
-                                    overflow: 'visible',
-                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                    mt: 1.5,
-                                    '&:before': {
-                                        content: '""',
-                                        display: 'block',
-                                        position: 'absolute',
-                                        top: 0,
-                                        right: 14,
-                                        width: 10,
-                                        height: 10,
-                                        bgcolor: 'background.paper',
-                                        transform: 'translateY(-50%) rotate(45deg)',
-                                        zIndex: 0,
-                                    },
-                                },
-                            }}
-                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                        >
-                            {userSettings.map((setting) => (
-                                <MenuItem
-                                    key={setting.title}
-                                    component={Link}
-                                    to={setting.url}
+                        {user ?
+                            <>
+                                <Button
+                                    className={classes.userBtn}
+                                    color='inherit'
+                                    size='large'
+                                    onClick={handleClick}
+                                    startIcon={<PersonIcon/>}
                                 >
-                                    <ListItemIcon>
-                                        {setting.icon}
-                                    </ListItemIcon>
-                                    <Typography textAlign="center">{setting.title}</Typography>
-                                </MenuItem>
-                            ))}
-                            <Divider/>
-                            {user.role === 'admin' && adminSettings.map(setting => (
-                                <MenuItem
-                                    key={setting.title}
-                                    component={Link}
-                                    to={setting.url}
-                                >
-                                    <ListItemIcon>
-                                        {setting.icon}
-                                    </ListItemIcon>
-                                    <Typography textAlign="center">{setting.title}</Typography>
-                                </MenuItem>
-                            ))}
-                            <Divider/>
-                            <MenuItem>
-                                <ListItemIcon>
-                                    <AccountBalanceWalletIcon/>
-                                </ListItemIcon>
-                                Ваш баланс {user?.balance + ' сом'}
-                            </MenuItem>
-                            <MenuItem onClick={toLogOut}>
-                                <ListItemIcon>
-                                    <Logout fontSize="small" />
-                                </ListItemIcon>
-                                Выйти
-                            </MenuItem>
-                        </Menu>
+                                    {user?.name}
+                                </Button>
+
+                                <UserMenu
+                                    user={user}
+                                    open={open}
+                                    toLogOut={toLogOut}
+                                    handleClose={handleClose}
+                                    anchorEl={anchorEl}
+                                />
+                            </>
+                            :
+                            <Anonymous/>
+                        }
                     </Box>
                 </Toolbar>
             </Container>
