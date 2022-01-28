f71f70083122e05c82a5b610b71d151e2ae3fa73
Shabdan <schabkul@gmail.com>
2022-01-29 05:20:32 +0600

    #37 Переделан BurgerMenu, использован material ui, теперь содержит кнопки навигации

--- frontend/src/components/UI/BurgerMenu/BurgerMenu.js
+++ frontend/src/components/UI/BurgerMenu/BurgerMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Box,
     IconButton,
@@ -13,13 +13,14 @@ import {makeStyles} from "@mui/styles";
 import CloseIcon from '@mui/icons-material/Close';
 import {Link} from "react-router-dom";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import MenuIcon from "@mui/icons-material/Menu";
 
 const useStyles = makeStyles({
     paper: {
         width: '80vw',
         height: '100%',
         zIndex: 99,
-        color: '#F5F5F7'
+        color: '#F5F5F7',
     },
     contentPaper: {
         background: '#424245',
@@ -28,57 +29,71 @@ const useStyles = makeStyles({
     }
 })
 
-const BurgerMenu = ({open, setOpen, pages}) => {
+const BurgerMenu = ({pages}) => {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
 
     return (
-        <SwipeableDrawer
-            anchor='left'
-            open={open}
-            onClose={() => setOpen(false)}
-            onOpen={() => {}}
-        >
-            <Box
-                className={classes.paper}
-                onKeyDown={() => setOpen(false)}
+        <>
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={() => setOpen(true)}
+                color="inherit"
             >
-                <Box>
-                    <Toolbar sx={{background: 'rgba(0,0,0,0.82)'}}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={() => setOpen(false)}
-                            color="inherit"
-                        >
-                            <CloseIcon />
-                        </IconButton>
-                    </Toolbar>
-                </Box>
+                <MenuIcon />
+            </IconButton>
 
-                <Box className={classes.contentPaper}>
-                    <List>
-                        {pages.map(item => (
-                            <ListItemButton
-                                key={item.title}
-                                sx={{color: "#F5F5F7"}}
-                                component={Link}
-                                to={item.url}
+            <SwipeableDrawer
+                anchor='left'
+                open={open}
+                onClose={() => setOpen(false)}
+                onOpen={() => {}}
+            >
+                <Box
+                    className={classes.paper}
+                    onKeyDown={() => setOpen(false)}
+                >
+                    <Box>
+                        <Toolbar sx={{background: 'rgba(0,0,0,0.82)'}}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
                                 onClick={() => setOpen(false)}
+                                color="inherit"
                             >
-                                <ListItemIcon>
-                                    <ArrowForwardIosIcon sx={{color: "#F5F5F7"}}/>
-                                </ListItemIcon>
-                                <ListItemText>
-                                    {item.title}
-                                </ListItemText>
-                            </ListItemButton>
-                        ))}
-                    </List>
+                                <CloseIcon />
+                            </IconButton>
+                        </Toolbar>
+                    </Box>
+
+                    <Box className={classes.contentPaper}>
+                        <List>
+                            {pages.map(item => (
+                                <ListItemButton
+                                    key={item.title}
+                                    sx={{color: "#F5F5F7"}}
+                                    component={Link}
+                                    to={item.url}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    <ListItemIcon>
+                                        <ArrowForwardIosIcon sx={{color: "#F5F5F7"}}/>
+                                    </ListItemIcon>
+                                    <ListItemText>
+                                        {item.title}
+                                    </ListItemText>
+                                </ListItemButton>
+                            ))}
+                        </List>
+                    </Box>
                 </Box>
-            </Box>
-        </SwipeableDrawer>
+            </SwipeableDrawer>
+        </>
     );
 };
 
