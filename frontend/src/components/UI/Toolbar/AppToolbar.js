49b6cf666e14757f55ba388515108b7545833a00
Shabdan <schabkul@gmail.com>
2022-01-29 05:21:00 +0600

    #37 Переделан AppToolbar, использован material ui

--- frontend/src/components/UI/Toolbar/AppToolbar.js
+++ frontend/src/components/UI/Toolbar/AppToolbar.js
@@ -1,21 +1,12 @@
-import React, {useState} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {AppBar, Box, Button, Divider, IconButton, ListItemIcon, Menu, MenuItem, Toolbar, Tooltip} from "@mui/material";
+import React from 'react';
+import {useSelector} from "react-redux";
+import {AppBar, Box, Button, Toolbar} from "@mui/material";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
-import MenuIcon from '@mui/icons-material/Menu';
 import {Link, useNavigate} from "react-router-dom";
 import logo from '../../../assets/logo.svg';
 import {makeStyles} from "@mui/styles";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import HistoryIcon from "@mui/icons-material/History";
-import AddIcon from "@mui/icons-material/Add";
-import FlightIcon from "@mui/icons-material/Flight";
-import PersonIcon from '@mui/icons-material/Person';
-import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
-import {Logout} from "@mui/icons-material";
-import {logout} from "../../../store/actions/usersActions";
 import UserMenu from "./Menu/UserMenu";
 import Anonymous from "./Menu/Anonymous";
 
@@ -37,37 +28,18 @@ const useStyles = makeStyles({
         fontSize: '22px',
         display: 'flex',
         alignItems: 'center'
-    },
-    userBtn: {
-        color: '#F5F5F7',
     }
 })
 
 const AppToolbar = () => {
-    const dispatch = useDispatch();
     const classes = useStyles();
     const navigate = useNavigate();
-    const [openNav, setOpenNav] = useState(false);
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
+
     const user = useSelector(state => state.users.user);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
 
     const navigateTo = (url) => {
         navigate(url);
     }
-
-    const toLogOut = () => {
-        setAnchorEl(false);
-        dispatch(logout());
-    };
 
     return (
         <AppBar position="static" sx={{background: 'rgba(0,0,0,0.82)'}}>
@@ -86,17 +58,7 @@ const AppToolbar = () => {
                     </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={() => setOpenNav(true)}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <BurgerMenu open={openNav} setOpen={setOpenNav} pages={pages}/>
+                        <BurgerMenu pages={pages}/>
                     </Box>
 
                     <Typography
@@ -125,25 +87,7 @@ const AppToolbar = () => {
 
                     <Box sx={{ flexGrow: 0 }}>
                         {user ?
-                            <>
-                                <Button
-                                    className={classes.userBtn}
-                                    color='inherit'
-                                    size='large'
-                                    onClick={handleClick}
-                                    startIcon={<PersonIcon/>}
-                                >
-                                    {user?.name}
-                                </Button>
-
-                                <UserMenu
-                                    user={user}
-                                    open={open}
-                                    toLogOut={toLogOut}
-                                    handleClose={handleClose}
-                                    anchorEl={anchorEl}
-                                />
-                            </>
+                            <UserMenu user={user}/>
                             :
                             <Anonymous/>
                         }
