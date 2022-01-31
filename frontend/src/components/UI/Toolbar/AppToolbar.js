8ea519b701376b094ace10ea30e0aad71bf65617
Shabdan <schabkul@gmail.com>
2022-01-31 20:26:50 +0600

    #37 Переделан AppToolbar, использован material ui, добавлен скролл эффект

--- frontend/src/components/UI/Toolbar/AppToolbar.js
+++ frontend/src/components/UI/Toolbar/AppToolbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-import {AppBar, Box, Button, Toolbar} from "@mui/material";
+import {AppBar, Box, Button, CssBaseline, Slide, Toolbar, useScrollTrigger} from "@mui/material";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
@@ -20,6 +20,19 @@ const pages = [
     {url: '/faq', title: 'FAQ'},
 ];
 
+function HideOnScroll(props) {
+    const { children, window } = props;
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+    });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
 const useStyles = makeStyles({
     logo: {
         color: '#F5F5F7',
@@ -31,7 +44,7 @@ const useStyles = makeStyles({
     }
 })
 
-const AppToolbar = () => {
+const AppToolbar = (props) => {
     const classes = useStyles();
     const navigate = useNavigate();
 
@@ -42,59 +55,65 @@ const AppToolbar = () => {
     }
 
     return (
-        <AppBar position="static" sx={{background: 'rgba(0,0,0,0.82)'}}>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                    >
-                        <Link to="/" className={classes.logo}>
-                            <img src={logo} alt="logo" style={{width: "40px"}}/>
-                            <span>Alga-Express</span>
-                        </Link>
-                    </Typography>
+        <>
+            <CssBaseline/>
+            <HideOnScroll {...props}>
+            <AppBar sx={{background: 'rgba(0,0,0,0.82)'}}>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                        >
+                            <Link to="/" className={classes.logo}>
+                                <img src={logo} alt="logo" style={{width: "40px"}}/>
+                                <span>Alga-Express</span>
+                            </Link>
+                        </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <BurgerMenu pages={pages}/>
-                    </Box>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <BurgerMenu pages={pages}/>
+                        </Box>
 
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                    >
-                        <Link to="/" className={classes.logo} >
-                            <img src={logo} alt="logo" style={{width: "40px"}}/>
-                            <span>Alga-Express</span>
-                        </Link>
-                    </Typography>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                        >
+                            <Link to="/" className={classes.logo} >
+                                <img src={logo} alt="logo" style={{width: "40px"}}/>
+                                <span>Alga-Express</span>
+                            </Link>
+                        </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page.title}
-                                onClick={() => navigateTo(page.url)}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page.title}
-                            </Button>
-                        ))}
-                    </Box>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page.title}
+                                    onClick={() => navigateTo(page.url)}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page.title}
+                                </Button>
+                            ))}
+                        </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        {user ?
-                            <UserMenu user={user}/>
-                            :
-                            <Anonymous/>
-                        }
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                        <Box sx={{ flexGrow: 0 }}>
+                            {user ?
+                                <UserMenu user={user}/>
+                                :
+                                <Anonymous/>
+                            }
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            </HideOnScroll>
+            <Toolbar />
+        </>
     );
 };
 
