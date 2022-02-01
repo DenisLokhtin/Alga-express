fd747f58c3144a7cf65b19cf937f424b418acdb8
Vitaliy Ten <vitalten10@gmail.com>
2022-02-01 18:14:08 +0600

    подготовка к работе по приемочным тестам

--- api/index.js
+++ api/index.js
@@ -10,13 +10,13 @@ const market=require('./app/market');
 const config = require('./config');
 const packages = require('./app/packages');
 const flight = require('./app/flights');
+const administration = require('./app/administration');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 
-const port = 8000;
 
 app.use('/news', news);
 app.use('/users', users);
@@ -24,12 +24,13 @@ app.use('/market',market);
 app.use('/packages', packages);
 app.use('/userEdit', userEdit);
 app.use('/flights', flight);
+app.use('/cargo', administration);
 
 const run = async () => {
     await mongoose.connect(config.db.url);
 
-    app.listen(port, () => {
-        console.log('Port start on port: ', port);
+    app.listen(config.port, () => {
+        console.log('Port start on port: ', config.port);
     });
 
     exitHook(() => {
