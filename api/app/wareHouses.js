2eae4840487f895c1870cb9897ad7960639ed273
aizhan <a.s.nogoeva@gmail.com>
2022-02-11 01:35:38 +0600

    #56 added api routes for warehouse

---
+++ api/app/wareHouses.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const Warehouse = require('../models/WareHouse');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+    try {
+        const warehouses = await Warehouse.find({deleted: {$ne : true}});
+        res.send(warehouses);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const warehouse = await Warehouse.findById(req.params.id);
+
+        if (warehouse) {
+            res.send(warehouse);
+        } else {
+            res.status(404).send({error: 'Warehouse not found'});
+        }
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    try {
+        const warehouseData = {
+            //тут будут поля для добавления
+        };
+
+        const warehouse = new Warehouse(warehouseData);
+
+        await warehouse.save();
+        res.send(warehouse);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const warehouse = await Warehouse.findById(req.params.id);
+
+        if (Object.keys(warehouse).length === 0) {
+            return res.status(404).send({error: `Склад не найден.`});
+        } else {
+            warehouse.deleted = true;
+            await warehouse.save();
+            return  res.send({message: `Склад в городе ${warehouse.city} успешно удален.`})
+        }
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedWarehouse = await Warehouse.findByIdAndUpdate(req.params.id, {
+            //обновляемые поля
+        }, {new: true, runValidators: true});
+
+        res.send(updatedWarehouse);
+
+    } catch(error) {
+        res.status(400).send(error);
+    }
+});
+
+module.exports = router;
--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ const flight = require('./app/flights');
 const buyouts = require('./app/buyouts');
 const administration = require('./app/administration');
 const userPayments = require('./app/userPayments');
+const wareHouses = require('./app/wareHouses');
 
 const app = express();
 app.use(express.json());
@@ -29,6 +30,7 @@ app.use('/flights', flight);
 app.use('/buyouts', buyouts);
 app.use('/cargo', administration);
 app.use('/payments', userPayments);
+app.use('/warehouses', wareHouses);
 
 const run = async () => {
     await mongoose.connect(config.db.url);
