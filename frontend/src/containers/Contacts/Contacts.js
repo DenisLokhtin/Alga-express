8f10f3e3391505b88646a5f61e5edf77265f6613
aizhan <a.s.nogoeva@gmail.com>
2022-02-20 23:32:45 +0600

    #56 added google-map for contact page

--- frontend/src/containers/Contacts/Contacts.js
+++ frontend/src/containers/Contacts/Contacts.js
@@ -20,6 +20,12 @@ const Contacts = () => {
             }}
                        component='div'>
                 <div className="post__content" dangerouslySetInnerHTML={{__html: page.text}}/>
+
+                <iframe
+                    title="google-map"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2925.4971004204913!2d74.62072151589662!3d42.841235779157536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb6697927eb87%3A0x4116c9c353dd9377!2zMTQyINC_0YDQvtGB0L8uINCu0L3Rg9GB0LDQu9C40LXQstCwLCDQkdC40YjQutC10Lo!5e0!3m2!1sru!2skg!4v1645193536509!5m2!1sru!2skg"
+                    width="300" height="200" style={{border: 0}} allowFullScreen="" loading="lazy">Адрес
+                </iframe>
             </Container>
         </Container>
     )
